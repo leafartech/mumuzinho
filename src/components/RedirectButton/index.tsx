f1dc44b97bd5d99a44be59735f9408605cacb87e
leafartech <119262053+leafartech@users.noreply.github.com>
2023-09-30 16:26:56 -0300

    Add files via upload

---
+++ src/components/RedirectButton/index.tsx
@@ -0,0 +1,7 @@
+export default function RedirectButton() {
+    return (
+        <div className="flex w-full">
+            <a href="#oferta" className="text-white w-full sm:max-w-xs text-center grad-3 text-lg py-3 px-3 font-bold rounded-lg">Quero aprender com o Juninho</a>
+        </div>
+    )
+}
